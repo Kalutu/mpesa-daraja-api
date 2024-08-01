0e40dff92262aa5a5293a7ebc92ce7266ec8e9ba
Kalutu <kalutudaniel@gmail.com>
2024-08-01 11:57:48 +0300

    Initial Set up

--- mpesa/apps.py
+++ mpesa/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DarajaConfig(AppConfig):
+class MpesaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'daraja'
+    name = 'mpesa'
