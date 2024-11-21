b39079e73ccfaaa731abbc697572c17575caaedf
Kalutu <kalutudaniel@gmail.com>
2024-11-21 12:23:45 +0300

    Limit the amount

--- 07. Compelete Code/Python (Django)/payments/mpesa_express/forms.py
+++ 07. Compelete Code/Python (Django)/payments/mpesa_express/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class PaymentForm(forms.Form):
     phone_number = forms.CharField(label='Phone Number', max_length=15)
-    amount = forms.IntegerField(label='Amount', min_value=1)
+    amount = forms.IntegerField(label='Amount', min_value=1, max_value=250000)
